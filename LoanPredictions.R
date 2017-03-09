35a881a52d23aab551c324c260a61a9f43ebe944
Manish Sihag <sihagmanish36@gmail.com>
2017-03-09 14:53:44 +0530

    Update LoanPredictions.R

--- LoanPredictions.R
+++ LoanPredictions.R
@@ -1,2 +1,8 @@
+```r
 train = read.csv("train.txt", sep = ",", header = T, na.strings = c("", NA))
 test = read.csv("test.txt", sep = ",", header = T, na.strings = c("", NA))
+summary(train)
+```
+```python
+import pandas as pd
+```
