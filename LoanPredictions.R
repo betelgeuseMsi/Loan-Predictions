d27faf6196f86dd006394c2a0407aeee94b1d990
betelgeuse <sihagmanish36@gmail.com>
2017-03-09 15:42:21 +0530

    simple

--- LoanPredictions.R
+++ LoanPredictions.R
@@ -1,8 +1,2 @@
-```r
 train = read.csv("train.txt", sep = ",", header = T, na.strings = c("", NA))
 test = read.csv("test.txt", sep = ",", header = T, na.strings = c("", NA))
-summary(train)
-```
-```python
-import pandas as pd
-```
